96eb8fe4e16870af167d455b87e45ba0ebbbba82
wang <857541619@qq.com>
2022-02-08 16:44:07 +0800

    2.5增加通用的返回类CommonResp，以后后端都按照改模板返回

--- src/main/java/com/wang/wiki1/controller/ebookController.java
+++ src/main/java/com/wang/wiki1/controller/ebookController.java
@@ -1,9 +1,13 @@
 package com.wang.wiki1.controller;
 
+import com.wang.wiki1.domain.Ebook;
+import com.wang.wiki1.resp.CommonResp;
 import com.wang.wiki1.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author zsw
@@ -14,9 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ebookController {
     @Autowired
     private EbookService ebookServer;
+
     @RequestMapping("/hello")
     public  String hello(){
 
         return  "hello2,"+ebookServer.list();
+    }
+    @RequestMapping("/resp")
+    public CommonResp resp(){
+        CommonResp<List<Ebook>> listCommonResp = new CommonResp<>();
+        listCommonResp.setContent(ebookServer.list());
+        return  listCommonResp;
     }
 }
