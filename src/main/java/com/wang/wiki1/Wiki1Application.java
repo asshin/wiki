e865073aa504561d6b96847134f97f7ff038a907
wang <857541619@qq.com>
2022-02-08 11:08:12 +0800

    1.项目初始化

---
+++ src/main/java/com/wang/wiki1/Wiki1Application.java
@@ -0,0 +1,13 @@
+package com.wang.wiki1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wiki1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wiki1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wang/wiki1/Wiki1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.wiki1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wiki1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
