a25943620e07931de416945c6bba251702e839a8
wang <857541619@qq.com>
2022-02-08 20:05:23 +0800

    3.2集成ant Design of vue

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <a-button type="dashed">
+      Dashed
+    </a-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
