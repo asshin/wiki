217b9f0f3af7ab079f31d944f65e387752d3e76e
wang <857541619@qq.com>
2022-02-08 11:47:29 +0800

    1.1项目初始化 1.2删除help.md增加readme.md 1.3增加日志文件，启动时打印端口号

--- src/main/java/com/wang/wiki1/Wiki1Application.java
+++ src/main/java/com/wang/wiki1/Wiki1Application.java
@@ -1,13 +1,27 @@
 package com.wang.wiki1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import static org.springframework.boot.Banner.Mode.LOG;
 
 @SpringBootApplication
 public class Wiki1Application {
 
+	private static final Logger LOG = LoggerFactory.getLogger(Wiki1Application.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(Wiki1Application.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(Wiki1Application.class, args);
+
+		Environment environment = run.getBean(Environment.class);
+//		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("地址: \thttp://127.0.0.1:{}", environment.getProperty("server.port"));
 	}
 
 }
+
