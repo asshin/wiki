d0d42d9ea183197e484f08958a0d64bf92aebd63
wang <857541619@qq.com>
2022-02-08 13:14:15 +0800

    1.1项目初始化 1.2删除help.md增加readme.md 1.3增加日志文件，启动时打印端口号 1.4把启动类放到config下面，并自定义扫描包路径，测试hello的访问 1.5增加热部署，把配置文件放到config下，优先级高于resources下

--- src/main/java/com/wang/wiki1/controller/testController.java
+++ src/main/java/com/wang/wiki1/controller/testController.java
@@ -1,5 +1,6 @@
 package com.wang.wiki1.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2022-02-08 11:50
  */
 @RestController
+
 public class testController {
+    @Value("${test.name:wang}")
+    private String  name;
     @RequestMapping("/hello")
     public  String hello(){
 
-        return  "hello";
+        return  "hello1,"+name;
+    }
+    @RequestMapping("/hello/post")
+    public  String helloPost(String name){
+
+        return  "hello,"+name;
     }
 }
