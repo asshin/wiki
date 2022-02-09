6ed2d99d5240d75afbff8007a97797b07f7be52d
wang <857541619@qq.com>
2022-02-09 21:04:09 +0800

    4.4电子书布局调整一行显示三列

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -39,7 +39,7 @@
     <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+      <a-list item-layout="vertical" size="large"  :grid="{ gutter: 20,column:3 }" :data-source="books">
         <template #footer>
           <div>
             <b>ant design vue</b>
@@ -47,25 +47,19 @@
           </div>
         </template>
         <template #renderItem="{ item }">
-          <a-list-item key="item.title">
+          <a-list-item key="item.name">
             <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
             <component v-bind:is="type" style="margin-right: 8px" />
             {{ text }}
           </span>
             </template>
-            <template #extra>
-              <img
-                      width="272"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-              />
-            </template>
+
             <a-list-item-meta :description="item.description">
               <template #title>
-                <a :href="item.href">{{ item.title }}</a>
+                <a :href="item.href">{{ item.name }}</a>
               </template>
-              <template #avatar><a-avatar :src="item.avatar" /></template>
+              <template #avatar><a-avatar :src="item.cover" /></template>
             </a-list-item-meta>
             {{ item.content }}
           </a-list-item>
