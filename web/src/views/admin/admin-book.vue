b802eb691fcd50d1715724ef7501b96d7c4c4319
wang <857541619@qq.com>
2022-02-10 14:51:24 +0800

    5.6.增加编辑的模态框

--- web/src/views/admin/admin-book.vue
+++ web/src/views/admin/admin-book.vue
@@ -22,7 +22,7 @@
             </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary">
+                <a-button type="primary" @click="edit">
                   编辑
                 </a-button>
                 <a-button type="danger">
@@ -36,6 +36,14 @@
 
     </a-layout-content>
   </a-layout>
+  <a-modal
+          title="电子书表单"
+          v-model:visible="modalVisible"
+          :confirm-loading="modalLoading"
+          @ok="handleModalOk"
+  >
+    <p>test</p>
+  </a-modal>
 </template>
 <script lang="ts">
   import { defineComponent, onMounted, ref } from 'vue';
@@ -121,6 +129,23 @@
           size: pagination.pageSize
         });
       };
+      // -------- 表单 ---------
+      const modalVisible = ref(false);
+      const modalLoading = ref(false);
+      const handleModalOk = () => {
+        modalLoading.value = true;
+        setTimeout(() => {
+          modalVisible.value = false;
+          modalLoading.value = false;
+        }, 2000);
+      };
+
+      /**
+       * 编辑
+       */
+      const edit = () => {
+        modalVisible.value = true;
+      };
 
       onMounted(() => {
         handleQuery({
@@ -134,7 +159,11 @@
         pagination,
         columns,
         loading,
-        handleTableChange
+        handleTableChange,
+        edit,
+        modalVisible,
+        modalLoading,
+        handleModalOk
       }
     }
   });
