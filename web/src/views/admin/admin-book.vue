cec9d0dc261af5333112059152a60ed96f6a06c3
wang <857541619@qq.com>
2022-02-10 15:02:38 +0800

    5.7编辑某一行可以将这一行的值传到模态框

--- web/src/views/admin/admin-book.vue
+++ web/src/views/admin/admin-book.vue
@@ -22,7 +22,7 @@
             </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary" @click="edit">
+                <a-button type="primary" @click="edit(record)">
                   编辑
                 </a-button>
                 <a-button type="danger">
@@ -42,7 +42,23 @@
           :confirm-loading="modalLoading"
           @ok="handleModalOk"
   >
-    <p>test</p>
+    <a-form :model="ebook" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover" />
+      </a-form-item>
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name" />
+      </a-form-item>
+      <a-form-item label="分类一">
+        <a-input v-model:value="ebook.category1Id" />
+      </a-form-item>
+      <a-form-item label="分类二">
+        <a-input v-model:value="ebook.category2Id" />
+      </a-form-item>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.desc" type="textarea" />
+      </a-form-item>
+    </a-form>
   </a-modal>
 </template>
 <script lang="ts">
@@ -130,6 +146,7 @@
         });
       };
       // -------- 表单 ---------
+      const ebook = ref({});
       const modalVisible = ref(false);
       const modalLoading = ref(false);
       const handleModalOk = () => {
@@ -143,8 +160,10 @@
       /**
        * 编辑
        */
-      const edit = () => {
+      const edit = (record:any) => {
+        console.log("record is this:"+record)
         modalVisible.value = true;
+        ebook.value=record
       };
 
       onMounted(() => {
@@ -155,6 +174,7 @@
       });
 
       return {
+        ebook,
         ebooks,
         pagination,
         columns,
